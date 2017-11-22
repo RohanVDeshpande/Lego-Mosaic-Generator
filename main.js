25d8c30d30344aebf6cc669fe0c68bfbc1382085
RohanVDeshpande <rohanvdeshpande@hotmail.com>
2017-11-21 19:58:11 -0500

    Added Toolkit SVG

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
-const url = require('url')
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -9,7 +9,6 @@ let win
 function createWindow () {
   // Create the browser window.
   win = new BrowserWindow({width: 1600, height: 800})
-
   // and load the index.html of the app.
   win.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
