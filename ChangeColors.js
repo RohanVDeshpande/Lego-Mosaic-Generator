3beab04665e378a7a245ed52c754530db9b32a06
RohanVDeshpande <rohanvdeshpande@hotmail.com>
2017-11-26 20:33:18 -0500

    Fill Color UI Styling

--- ChangeColors.js
+++ ChangeColors.js
@@ -1,10 +1,21 @@
-$(document).on('click', '.usedColor', function () {
-	$('.selectedColor').removeClass('selectedColor');
-	$(this).addClass('selectedColor');
-});
+//Generate Color Palette in HTML
 $(document).ready(function(){
 	var colors = [[39, 37, 31],[217, 217, 214],[100, 100, 100], [150, 150, 150], [55, 33, 0], [170, 125, 85], [137, 125, 98], [176, 160, 111], [0, 69, 26], [0, 133, 43], [112, 142, 124], [88, 171, 65], [252, 172, 0], [214, 121, 35], [30, 90, 168], [70, 155, 195], [157, 195, 247], [114, 0, 18], [180, 0, 0], [95, 49, 9]];
 	$.each(colors, function(index, value){
 		$('#colorPalette').append("<li class='colorPalElm' style='background-color:rgb("+value+")'></li>");
 	});
 })
+
+
+//Click Colors that were used in image
+$(document).on('click', '.usedColor', function () {
+	$('.selectedColor').removeClass('selectedColor');
+	$(this).addClass('selectedColor');
+});
+
+//Click Colors from color palette
+$(document).on('click', '.colorPalElm', function () {
+	$('.colorPalSelected').removeClass('colorPalSelected');
+	$(this).addClass('colorPalSelected');
+});
+
